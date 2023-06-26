8525355dd6411fe96c491ab04401225dc78c182c
judasaca <salamancajd14@gmail.com>
2023-06-26 14:39:27 -0500

    Full day 1 exercise.

--- days/day1/script.js
+++ days/day1/script.js
@@ -4,6 +4,23 @@ document.addEventListener("keydown", (event) => {
   const key = event.key;
   const avaliableKeys = ["q", "w", "e", "r", "t", "y", "u", "i"];
   if (avaliableKeys.includes(key)) {
-    document.getElementById(key).getElementsByTagName("audio")[0].play();
+    const box = document.getElementById(key);
+    const audio = box.getElementsByTagName("audio")[0];
+    audio.currentTime = 0;
+    audio.play();
+    box.classList.add("playing");
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  const key = event.key;
+  const avaliableKeys = ["q", "w", "e", "r", "t", "y", "u", "i"];
+  if (avaliableKeys.includes(key)) {
+    const box = document.getElementById(key);
+    const audio = box.getElementsByTagName("audio")[0];
+    // audio.play();
+    // audio.pause();
+    // audio.currentTime = 0;
+    box.classList.remove("playing");
   }
 });
